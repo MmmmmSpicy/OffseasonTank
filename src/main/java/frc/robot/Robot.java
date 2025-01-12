0c9599c487e309bbb400986f3d45d24a03a957fc
MmmmmSpicy <ethanspickard@gmail.com>
2025-01-11 20:44:29 -0500

    Updated to 2025

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,12 @@
 
 package frc.robot;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Joystick;
@@ -18,18 +21,32 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  * arcade steering.
  */
 public class Robot extends TimedRobot {
-  private final CANSparkMax Leaderleftmotor = new CANSparkMax(2, MotorType.kBrushless);
-  private final CANSparkMax Leaderrightmotor = new CANSparkMax(4, MotorType.kBrushless);
-  private final CANSparkMax Followerleftmotor = new CANSparkMax(1, MotorType.kBrushless);
-  private final CANSparkMax Followerrightmotor = new CANSparkMax(3, MotorType.kBrushless);
-  
+  private final int idLeaderleft = 2;
+  private final int idLeaderright = 4;
+  private final int idFollowerleft = 1;
+  private final int idFollowerright = 3;
+
+  private final boolean rightInverted = true;
+  private final boolean leftInverted = false;
+
+  private final SparkMax motorLeaderleft = new SparkMax(idLeaderleft, MotorType.kBrushless);
+  private final SparkMax motorLeaderright = new SparkMax(idLeaderright, MotorType.kBrushless);
+  private final SparkMax motorFollowerleft = new SparkMax(idFollowerleft, MotorType.kBrushless);
+  private final SparkMax motorFollowerright = new SparkMax(idFollowerright, MotorType.kBrushless);
+
+  private final SparkMaxConfig configLeaderleft = new SparkMaxConfig();
+  private final SparkMaxConfig configLeaderright = new SparkMaxConfig();
+  private final SparkMaxConfig configFollowerleft = new SparkMaxConfig();
+  private final SparkMaxConfig configFollowerright = new SparkMaxConfig();
+
+
   private final DifferentialDrive robotDrive =
-      new DifferentialDrive(Leaderleftmotor::set, Leaderrightmotor::set);
+      new DifferentialDrive(motorLeaderleft::set, motorLeaderright::set);
   private final Joystick stick = new Joystick(0);
 
   public Robot() {
-    SendableRegistry.addChild(robotDrive, Leaderleftmotor);
-    SendableRegistry.addChild(robotDrive, Leaderrightmotor);
+    SendableRegistry.addChild(robotDrive, motorLeaderleft);
+    SendableRegistry.addChild(robotDrive, motorLeaderright);
   }
 
   @Override
@@ -37,14 +54,23 @@ public class Robot extends TimedRobot {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    Leaderrightmotor.setInverted(true);
-    Followerrightmotor.setInverted(true);
-    Followerleftmotor.follow(Leaderleftmotor);
-    Followerrightmotor.follow(Leaderrightmotor);
-    Leaderleftmotor.setIdleMode(IdleMode.kBrake);
-    Leaderrightmotor.setIdleMode(IdleMode.kBrake);
-    Followerleftmotor.setIdleMode(IdleMode.kBrake);
-    Followerrightmotor.setIdleMode(IdleMode.kBrake);
+
+    configLeaderright.inverted(leftInverted);
+    configFollowerright.inverted(rightInverted);
+    
+    configFollowerleft.follow(idLeaderleft);
+    configFollowerright.follow(idLeaderright);
+    
+    configLeaderleft.idleMode(IdleMode.kBrake);
+    configLeaderright.idleMode(IdleMode.kBrake);
+    configFollowerleft.idleMode(IdleMode.kBrake);
+    configFollowerright.idleMode(IdleMode.kBrake);
+
+    motorLeaderleft.configure(configLeaderleft, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motorLeaderright.configure(configLeaderright, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motorFollowerleft.configure(configFollowerleft, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motorFollowerright.configure(configFollowerright, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
   }
 
   @Override
